425d1d2b4cc7173847c2b3648bec2f3e189bf43d
Selvanayaki S <selvanayaki060207@gmail.com>
2026-09-25 22:05:14 +0530

    Updated java

--- sum.java
+++ sum.java
@@ -4,11 +4,12 @@ public class sum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int n = sc.nextInt();
 
-        int sum = a + b;
-
-        System.out.println(sum);
+        if (n % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
     }
 }
